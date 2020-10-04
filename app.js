8e56fc05a47251af269ad943cc2d1cad41e4904c
Atish Gupta <atishgupta676@gmail.com>
2020-10-04 11:08:04 +0530

    updated

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 	'use strict';//means it'll be strict with us to use every single thing like var, etc.
 	
 	angular.module('nameCalculator', [])
-	.controller('nameCalculatorController', controller);
-		controller.$inject = ['$scope', '$filter'];
+	.controller('nameCalculatorController'/*name*/, controller/*function name*/)
+	.filter('likes', likesFilter)
+	.filter('truth', truthFilter);
+	
+		controller.$inject = ['$scope', '$filter', 'likesFilter'];/*May seem like function name but custom filters are injected are given with nameFilter like atishFilter, lovesFilter, etc. i.e. Filter is necessary to add at end.*/
 		
-		function controller($scope, $filter){
+		function controller($scope, $filter, likesFilter){/*Same here*/
 			$scope.name = "";
 			$scope.finalValue = 0;
+			$scope.plate = 50;
 		
 			$scope.calcValue = function(){
 				$scope.finalValue = calcStringValue($scope.name);
@@ -26,9 +30,37 @@
 				$scope.name = upCase($scope.name);
 			};
 			
-			$scope.number = "ua2";
+			$scope.number = "ua4";
 			$scope.changeImage = function(){
 				$scope.number = "ua3";
+			};
+			
+			$scope.msg = function(){
+				var msg = "Atish loves Rajma Chawal :P";
+				var output = $filter('uppercase')(msg);
+				return output;
+			};
+			
+			$scope.likesmsg = function(){
+				var msg = "Atish loves Rajma Chawal :P";
+				msg = likesFilter(msg);
+				return msg;
+			};
+		}
+		
+		function likesFilter(){
+			return function(input){
+				input = input || "";
+				input = input.replace("loves","likes");
+				return input;
+			};
+		}
+		
+		function truthFilter(){
+			return function(input, target, replace){
+				input = input || "";
+				input = input.replace(target, replace);
+				return input;
 			};
 		}
 })();
